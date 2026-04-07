37d7db14e26c96c3fddfed3011b2b78b208e7da7
Mohamedaminekartibou <mohamedaminekartibou@paucasesnovescifp.cat>
2026-04-07 21:06:35 +0200

    Refactor menu toggle functionality on DOMContentLoaded

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-function toggleMenu() {
-    document.querySelector(".main-menu").classList.toggle("activo");
-}
 
-document.querySelector(".tresBtotnes").addEventListener("click", function() {
-    document.querySelector(".main-menu").classList.toggle("activo");
-});
+document.addEventListener("DOMContentLoaded", () => {
 
+    const menu = document.querySelector(".main-menu");
+    const boton = document.querySelector(".tresBtotnes");
+    const overlay = document.querySelector(".overlay");
 
-const menu = document.querySelector(".main-menu");
-const boton = document.querySelector(".tresBtotnes");
-const overlay = document.querySelector(".overlay");
+    boton.addEventListener("click", () => {
+        menu.classList.toggle("activo");
+        boton.classList.toggle("activo");
+        overlay.classList.toggle("activo");
+    });
 
-boton.addEventListener("click", () => {
-    menu.classList.toggle("activo");
-    boton.classList.toggle("activo");
-    overlay.classList.toggle("activo");
-});
+    overlay.addEventListener("click", () => {
+        menu.classList.remove("activo");
+        boton.classList.remove("activo");
+        overlay.classList.remove("activo");
+    });
 
-overlay.addEventListener("click", () => {
-    menu.classList.remove("activo");
-    boton.classList.remove("activo");
-    overlay.classList.remove("activo");
 });
