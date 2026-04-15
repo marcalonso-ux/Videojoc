7ae28b029ab0125e6b50a83b1cf8106dfbba700b
marcalonso@paucasesnovescifp.cat <marcalonsopol0708@gmail.com>
2026-04-15 09:47:30 +0200

    Creado inital commit

---
+++ Equip/scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const menu = document.querySelector(".main-menu");
+    const boton = document.querySelector(".tresBtotnes");
+    const overlay = document.querySelector(".overlay");
+
+    boton.addEventListener("click", () => {
+        menu.classList.toggle("activo");
+        boton.classList.toggle("activo");
+        overlay.classList.toggle("activo");
+    });
+
+    overlay.addEventListener("click", () => {
+        menu.classList.remove("activo");
+        boton.classList.remove("activo");
+        overlay.classList.remove("activo");
+    });
+
+});
