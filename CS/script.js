eae95234f2ba8e3deed42e8a2e9438e4b41c2d6f
Mohamedaminekartibou <mohamedaminekartibou@paucasesnovescifp.cat>
2026-05-10 16:30:21 +0200

    feat(js): Para cambiar de  modo claro / oscuro

--- CS/script.js
+++ CS/script.js
@@ -2,4 +2,46 @@ function IrInfo() {
     window.location.href = "info.html";
 }
 
-document.getElementById('btInfo').onclick = IrInfo;
+if(document.getElementById('btInfo')) {
+    document.getElementById('btInfo').onclick = IrInfo;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.querySelector(".main-menu");
+    const boton = document.querySelector(".tresBotones");
+    const overlay = document.querySelector(".overlay");
+
+    if (boton && menu && overlay) {
+        boton.addEventListener("click", () => {
+            menu.classList.toggle("activo");
+            boton.classList.toggle("activo");
+            overlay.classList.toggle("activo");
+        });
+
+        overlay.addEventListener("click", () => {
+            menu.classList.remove("activo");
+            boton.classList.remove("activo");
+            overlay.classList.remove("activo");
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const themeBtn = document.getElementById("themeToggle");
+    
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme === "light") {
+        document.body.classList.add("light-mode");
+    }
+
+    if(themeBtn) {
+        themeBtn.addEventListener("click", () => {
+            document.body.classList.toggle("light-mode");
+            let theme = "dark";
+            if (document.body.classList.contains("light-mode")) {
+                theme = "light";
+            }
+            localStorage.setItem("theme", theme);
+        });
+    }
+});
